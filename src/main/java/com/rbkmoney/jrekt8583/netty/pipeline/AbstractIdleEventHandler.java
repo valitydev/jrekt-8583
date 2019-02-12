62f1c3c7d82a661f7cb83895c20ee440222456ac
Pavel Popov <tolkonepiu@users.noreply.github.com>
2019-02-12 15:11:52 +0300

    AbstractIdleHandler now sharable (#10)

--- src/main/java/com/rbkmoney/jrekt8583/netty/pipeline/AbstractIdleEventHandler.java
+++ src/main/java/com/rbkmoney/jrekt8583/netty/pipeline/AbstractIdleEventHandler.java
@@ -2,11 +2,13 @@ package com.rbkmoney.jrekt8583.netty.pipeline;
 
 import com.solab.iso8583.IsoMessage;
 import com.solab.iso8583.MessageFactory;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 
+@ChannelHandler.Sharable
 public abstract class AbstractIdleEventHandler extends ChannelInboundHandlerAdapter {
 
     @Override
